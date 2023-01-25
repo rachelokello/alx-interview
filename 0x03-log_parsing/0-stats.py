5b13547d47bfc3d4149b1ce94ccc5804abb8696b
rachelokello <okelloawuor@gmail.com>
2023-01-25 12:24:09 +0300

    logparsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,52 +1,44 @@
 #!/usr/bin/python3
-
-'''
-Reads stdin line by line and computes metrics
-'''
+"""Log parsing"""
+import signal
 import sys
 
-if __name__ == "__main__":
 
-    status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
-                    403: 0, 404: 0, 405: 0, 500: 0}
-    file_size = [0]
-    count = 1
-
-    def print_stats():
-        '''
-        Prints file size and stats for every 10 loops
-        '''
-        print('File size: {}'.format(file_size[0]))
-
-        for code in sorted(status_codes.keys()):
-            if status_codes[code] != 0:
-                print('{}: {}'.format(code, status_codes[code]))
-
-    def parse_stdin(line):
-        '''
-        Checks the stdin for matches
-        '''
-        try:
-            line = line[:-1]
-            word = line.split(' ')
-            # File size is last parameter on stdout
-            file_size[0] += int(word[-1])
-            # Status code comes before file size
-            status_code = int(word[-2])
-            # Move through dictionary of status codes
-            if status_code in status_codes:
-                status_codes[status_code] += 1
-        except BaseException:
-            pass
+if __name__ == '__main__':
+    sum_file_size = 0
+    status_code = {'200': 0,
+                   '301': 0,
+                   '400': 0,
+                   '401': 0,
+                   '403': 0,
+                   '404': 0,
+                   '405': 0,
+                   '500': 0}
 
     try:
+        i = 0
         for line in sys.stdin:
-            parse_stdin(line)
-            # print the stats after every 10 outputs
-            if count % 10 == 0:
-                print_stats()
-            count += 1
+            args = line.split()
+            if len(args) > 6:
+                status_line = args[-2]
+                file_size = args[-1]
+                sum_file_size += int(file_size)
+                if status_line in status_code:
+                    i += 1
+                    status_code[status_line] += 1
+                    if i % 10 == 0:
+                        print('File size: {}'.format(sum_file_size))
+                        for key, value in sorted(status_code.items()):
+                            if value != 0:
+                                print('{}: {}'.format(key, value))
+
+        print('File size: {}'.format(sum_file_size))
+        for key, value in sorted(status_code.items()):
+            if value != 0:
+                print('{}: {}'.format(key, value))
+
     except KeyboardInterrupt:
-        print_stats()
-        raise
-    print_stats()
+        print('File size: {}'.format(sum_file_size))
+        for key, value in sorted(status_code.items()):
+            if value != 0:
+                print('{}: {}'.format(key, value))
